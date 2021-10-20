10e590d320f91474a061f197891967accb519021
MrPinoBoy <beaupain.thomas@gmail.com>
2021-10-20 15:34:21 +0200

    started styling + reset done

--- src/App.js
+++ src/App.js
@@ -1,20 +1,37 @@
 import React, {useState} from "react";
 import Timer from "./components/Timer";
 import Controls from "./components/Controls";
+import "./style.css";
 
 const App = () => {
     const [seconds, setSeconds] = useState(5);
-    const [start, setStart] = useState(false);
+    const [start, setStart] = useState("stop");
+    const [totalSeconds, settotalSeconds] = useState();
+    const [initialTime, setInitialTime] = useState(0);
     return (
         <>
-            <h2>{"Cool test B)"}</h2>
-            <p>{"Test"}</p>
+            <h2 className={"title"}>{"Pomodoro"}</h2>
+            <div className={"div"}>
+                <div className={"lune"} />
+                <div className={"lune-2"} id={"lune"} />
+                <div className={"soleil"} id={"soleil"} />
+                <div className={"soleil-2"} />
+            </div>
+            <div className={"nuage"}>
+                <div className={"nuage-bas"} />
+                <div className={"nuage-haut"} />
+                <div className={"nuage-haut2"} />
+            </div>
             <Timer seconds={seconds} setSeconds={setSeconds} />
             <Controls
                 seconds={seconds}
                 setSeconds={setSeconds}
                 start={start}
                 setStart={setStart}
+                totalSeconds={totalSeconds}
+                settotalSeconds={settotalSeconds}
+                initialTime={initialTime}
+                setInitialTime={setInitialTime}
             />
         </>
     );
--- src/components/Controls.js
+++ src/components/Controls.js
@@ -1,21 +1,51 @@
 import React, {useEffect} from "react";
 
 const Controls = props => {
-    if (props.seconds > 1 && props.start === true) {
-        useEffect(
-            () => setTimeout(() => props.setSeconds(props.seconds - 0.01), 10),
-            [props.seconds],
+    if (props.seconds > 1 && props.start === "play") {
+        useEffect(() =>
+            setTimeout(() => props.setSeconds(props.seconds - 0.1), 100),
         );
     }
+    const resetAnim = () => {
+        document.querySelector("#soleil").style.animation = "none";
+        document.querySelector("#lune").style.animation = "none";
+    };
     return (
         <>
             <button
+                id={"trigger"}
                 type={"button"}
                 onClick={() => {
-                    if (props.start === false) {
-                        props.setStart(true);
+                    console.log(props.seconds);
+                    if (props.start === "stop" && props.seconds > 1) {
+                        props.setInitialTime(props.seconds);
+                        props.setStart("play");
+                        document.querySelector(
+                            "#soleil",
+                        ).style.animation = `eclipse-1 ${
+                            props.seconds / 2
+                        }s ease-in forwards`;
+                        document.querySelector(
+                            "#lune",
+                        ).style.animation = `eclipse-2 ${props.seconds / 2}s ${
+                            props.seconds / 2
+                        }s ease-out forwards`;
+                    } else if (props.start === "pause") {
+                        props.setStart("play");
+                        document.querySelector(
+                            "#soleil",
+                        ).style.animationPlayState = "running";
+                        document.querySelector(
+                            "#lune",
+                        ).style.animationPlayState = "running";
                     } else {
-                        props.setStart(false);
+                        props.setStart("pause");
+                        document.querySelector(
+                            "#soleil",
+                        ).style.animationPlayState = "paused";
+                        document.querySelector(
+                            "#lune",
+                        ).style.animationPlayState = "paused";
                     }
                 }}>
                 {"Start/Pause"}
@@ -24,24 +54,34 @@ const Controls = props => {
             <button
                 type={"button"}
                 onClick={() => {
-                    props.setStart(false);
-                    setTimeout(() => props.setSeconds(props.seconds + 60), 10);
+                    resetAnim();
+                    props.setStart("stop");
+                    setTimeout(() => props.setSeconds(props.seconds + 27), 100);
                 }}>
                 {"+"}
             </button>
             <button
                 type={"button"}
                 onClick={() => {
-                    props.setStart(false);
-                    setTimeout(() => props.setSeconds(props.seconds - 60), 10);
+                    resetAnim();
+                    props.setStart("stop");
+                    if (props.seconds > 30) {
+                        setTimeout(
+                            () => props.setSeconds(props.seconds - 30),
+                            100,
+                        );
+                    } else {
+                        setTimeout(() => props.setSeconds(0), 100);
+                    }
                 }}>
                 {"-"}
             </button>
             <button
                 type={"button"}
                 onClick={() => {
-                    props.setStart(false);
-                    setTimeout(() => props.setSeconds(0), 10);
+                    resetAnim();
+                    props.setStart("stop");
+                    setTimeout(() => props.setSeconds(props.initialTime), 100);
                 }}>
                 {"Reset"}
             </button>
